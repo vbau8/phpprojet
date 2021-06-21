a8e0577a5b8fb50dfdf83d4db6df3f22d35b1112
STMIHAI84 <54702736+STMIHAI84@users.noreply.github.com>
2021-06-21 13:51:12 +0200

    Add files via upload

--- phpecomerce.sql
+++ phpecomerce.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : lun. 21 juin 2021 à 09:47
--- Version du serveur :  5.7.31
--- Version de PHP : 8.0.7
+-- Gazdă: 127.0.0.1
+-- Timp de generare: iun. 21, 2021 la 01:49 PM
+-- Versiune server: 10.4.19-MariaDB
+-- Versiune PHP: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,68 +18,132 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `phpecomerce`
+-- Bază de date: `phpecomerce`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `auteur`
+-- Structură tabel pentru tabel `author`
 --
 
-DROP TABLE IF EXISTS `auteur`;
-CREATE TABLE IF NOT EXISTS `auteur` (
+CREATE TABLE `author` (
   `id` int(11) NOT NULL,
-  `prenom` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nom` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `firstName` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `pdtnonperiss`
+-- Structură tabel pentru tabel `line`
 --
 
-DROP TABLE IF EXISTS `pdtnonperiss`;
-CREATE TABLE IF NOT EXISTS `pdtnonperiss` (
+CREATE TABLE `line` (
+  `id` int(11) NOT NULL,
+  `idShoppingCard` int(11) NOT NULL,
+  `idProduct` int(11) NOT NULL,
+  `qte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structură tabel pentru tabel `pdtnonperis`
+--
+
+CREATE TABLE `pdtnonperis` (
   `id` int(11) NOT NULL,
   `type` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `couleur` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `color` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `idAuteur` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `pdtperiss`
+-- Structură tabel pentru tabel `pdtperis`
 --
 
-DROP TABLE IF EXISTS `pdtperiss`;
-CREATE TABLE IF NOT EXISTS `pdtperiss` (
+CREATE TABLE `pdtperis` (
   `id` int(11) NOT NULL,
-  `dateLimiteCons` date NOT NULL,
-  `poids` double NOT NULL,
-  `parfum` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL
+  `dateExpir` date NOT NULL,
+  `weight` double NOT NULL,
+  `perfume` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'fragance',
+  `tempStorage` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `produit`
+-- Structură tabel pentru tabel `product`
 --
 
-DROP TABLE IF EXISTS `produit`;
-CREATE TABLE IF NOT EXISTS `produit` (
+CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `pdt_perissabl` int(11) NOT NULL,
   `pdt_non_preissabl` int(11) NOT NULL,
   `libelle` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
   `marque` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `prixUnit` float DEFAULT NULL,
-  `QteStock` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `QteStock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structură tabel pentru tabel `shoppingcart`
+--
+
+CREATE TABLE `shoppingcart` (
+  `id` int(11) NOT NULL,
+  `product` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `amount` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexuri pentru tabele eliminate
+--
+
+--
+-- Indexuri pentru tabele `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexuri pentru tabele `line`
+--
+ALTER TABLE `line`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexuri pentru tabele `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexuri pentru tabele `shoppingcart`
+--
+ALTER TABLE `shoppingcart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pentru tabele eliminate
+--
+
+--
+-- AUTO_INCREMENT pentru tabele `line`
+--
+ALTER TABLE `line`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pentru tabele `shoppingcart`
+--
+ALTER TABLE `shoppingcart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

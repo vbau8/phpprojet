797bcef3956f6810062de03821f991f8ff70ccfd
vbau8 <83596294+vbau8@users.noreply.github.com>
2021-06-22 15:35:03 +0200

    Update scriptData.sql

--- scriptData.sql
+++ scriptData.sql
@@ -28,20 +28,20 @@ insert into pdtnonperis(type, couleur, idAuteur) values ('Anniversaire', NULL, 4
 insert into pdtnonperis(type, couleur, idAuteur) values ('Anniversaire', NULL, 5);	
 
 /* produit */
-insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('croissant beurre',NULL,1,25,1,NULL);
+insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('pain',NULL,1,25,1,NULL);
 insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('baguette',NULL,1.10,30,2,NULL);
 insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('stylo bille','BIC',2.2,100,NULL,1);
 insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('carte postale','pop carte',0.5,16,NULL,2);
 insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('pain','BIO',2.5,55,1,NULL);
 insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('pain céréal','BIO',2.4,20,1,NULL);
-insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('pain au chocolat',NULL,1.2,12,1,NULL);
+insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('pain sans sel',NULL,1.2,12,1,NULL);
 insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('parfum','DIOR',20,3,2,NULL);
 insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('glace',NULL, 10,2,2,NULL);
 insert into produit(libelle, marque, prixUnit, qteStock, pdtPeriss, pdtNonPeriss) values ('glace',NULL, 3,4,5,NULL);	
 /* panier */
-insert into panier(montant, refcli) values (100,1);
-insert into panier(montant, refcli) values (20,2);
-insert into panier(montant, refcli) values (10,3);
+insert into panier(montant, refClient) values (100,'1');
+insert into panier(montant, refClient) values (20,'2');
+insert into panier(montant, refClient) values (10,'3');
 /* ligne */
 insert into ligne (idProduit, idPanier, qte) values (1,1,5);
 insert into ligne (idProduit, idPanier, qte) values (2,1,2);
@@ -52,7 +52,3 @@ insert into ligne (idProduit, idPanier, qte) values (8,2,8);
 insert into ligne (idProduit, idPanier, qte) values (1,3,5);
 insert into ligne (idProduit, idPanier, qte) values (6,3,5);
 insert into ligne (idProduit, idPanier, qte) values (8,3,5);
-
-
-
-

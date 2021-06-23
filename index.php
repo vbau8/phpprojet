88a359f3a53c3b9d9c15b148427ab23fd3b79460
thperchi <thibault.perchicot@outlook.fr>
2021-06-23 14:58:40 +0200

    test affich produit ok

--- index.php
+++ index.php
@@ -3,13 +3,7 @@
 	require_once('classes/glace.php');
 	$g = new Glace('Hagen Daas', 'Cookie Dough', 80, 5, '10/01/2001', 'Cookie', 0);
 	$p = new Pain(50, 'Boule Ange Riz', 'Pain au cereales', 50, 4, 1);
-	echo $p->__toString();
-	echo $g->__toString();
-/*	session_start();
-	require_once("classes/produit");
-	$produit = new Produit();
-	$produits = $produits->selectAll();*/
-	$x = 10;
+	$produits = [$p, $g];
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
@@ -27,8 +21,8 @@
 					<div class="col-md-4">
 						<div class="card" style="width: 18rem;">
 							<div class="card-body">
-								<h5 class="card-title"><?php $p->marque?></h5>
-								<p class="card-text"><?php $p->libelle?></p>
+								<h5 class="card-title"><?php echo $p->marque?></h5>
+								<p class="card-text"><?php echo $p->libelle?></p>
 								<div class="row">
 									<a href="#" class="btn btn-primary">Ajouter au panier</a>
 									<a href="<?php echo 'produit.php/id='.$p->id;?>" class="btn btn-primary">Voir</a>

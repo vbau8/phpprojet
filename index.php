21efd2cf22c1c31f1700fcafaf4f0174bde2203e
thperchi <thibault.perchicot@outlook.fr>
2021-06-24 10:01:40 +0200

    yay

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	$p = new Pain(50, 'Boule Ange Riz', 'Pain au cereales', 50, 4, 1);
 	$produits = [$p, $g];
 ?>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
 	<body>
 		<section class="jumbotron text-center">

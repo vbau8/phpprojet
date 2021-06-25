440afaca8c3ff731c521f656e40a879051fc5ff4
thperchi <thibault.perchicot@outlook.fr>
2021-06-25 13:05:59 +0200

    view panier almost done

---
+++ base/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visualiser le panier</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">    
+</head>
+<body class="bg-light">
+    <section class="jumbotron text-center">
+        <h1 class="jumbotron-heading">Ma belle boutique</h1>
+        <p class="lead text-muted">Ma belle boutique propose à la vente différents articles (des cartes postales, des stylos mais aussi des produits régionaux : des pains et des glaces).</p>
+    </section>
